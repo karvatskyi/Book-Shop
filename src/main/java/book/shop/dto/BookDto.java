c42c1fe5784e7c54ceac85592c047a222177bff9
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-10-03 22:06:00 +0300

    add dto, add mapstruct, add controller with method create own excpetion

---
+++ src/main/java/book/shop/dto/BookDto.java
@@ -0,0 +1,14 @@
+package book.shop.dto;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class BookDto {
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
--- src/main/java/book/shop/repository/BookRepositoryImpl.java
+++ src/main/java/book/shop/repository/BookRepositoryImpl.java
@@ -1,16 +1,49 @@
 package book.shop.repository;
 
 import book.shop.model.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
+@Repository
 public class BookRepositoryImpl implements BookRepository {
+
+    private final EntityManagerFactory entityManagerFactory;
+
     @Override
     public Book save(Book book) {
-        return null;
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book: " + book + "to DB", e);
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return null;
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Book book = entityManager.find(Book.class,id);
+            return Optional.ofNullable(book);
+        }
     }
 }
