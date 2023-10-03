fd14779ff188f2cda3a513feaaec3d5bef50b5be
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-10-03 18:54:58 +0300

    create book entity, create book repo and service

---
+++ src/main/java/book/shop/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package book.shop.repository;
+
+import book.shop.model.Book;
+import java.util.List;
+
+public interface BookRepository {
+
+    Book save(Book book);
+
+    List<Book> findAll();
+
+}
---
+++ src/main/java/book/shop/repository/BookRepositoryImpl.java
@@ -0,0 +1,16 @@
+package book.shop.repository;
+
+import book.shop.model.Book;
+import java.util.List;
+
+public class BookRepositoryImpl implements BookRepository {
+    @Override
+    public Book save(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+    }
+}
